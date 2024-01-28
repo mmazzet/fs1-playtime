fc5bbf7a624071f16feb9b0ec769782e1cbe8948
Marianna Mazzette <20104120@mail.wit.ie>
2024-01-28 13:23:47 +0000

    get and delete tests

--- test/user-model-test.js
+++ test/user-model-test.js
@@ -25,4 +25,23 @@ suite("User API tests", () => {
     returnedUsers = await db.userStore.getAllUsers();
     assert.equal(returnedUsers.length, 0);
   });
+  test("get a user - success", async () => {
+    const user = await db.userStore.addUser(maggie);
+    const returnedUser1 = await db.userStore.getUserById(user._id);
+    assert.deepEqual(user, returnedUser1);
+    const returnedUser2 = await db.userStore.getUserByEmail(user.email);
+    assert.deepEqual(user, returnedUser2);
+  });
+
+  test("delete One User - success", async () => {
+    for (let i = 0; i < testUsers.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      testUsers[i] = await db.userStore.addUser(testUsers[i]);
+    }
+    await db.userStore.deleteUserById(testUsers[0]._id);
+    const returnedUsers = await db.userStore.getAllUsers();
+    assert.equal(returnedUsers.length, testUsers.length - 1);
+    const deletedUser = await db.userStore.getUserById(testUsers[0]._id);
+    assert.isNull(deletedUser);
+  });
 });
